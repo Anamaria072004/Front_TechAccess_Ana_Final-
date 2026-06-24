73ad7411cea3d62ddfd858296060c9d9f4aa83a8
Leidy <marcelaguerrero5069380@gmail.com>
2026-06-24 13:38:56 -0500

    auth-guards.ts

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,26 +1,15 @@
 import { inject } from '@angular/core';
 import { Router, CanActivateFn } from '@angular/router';
 import { Auth } from '../services/auth'; 
-import { map, tap } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(Auth);
   const router = inject(Router);
 
- 
   if (authService.isAuthenticated()) {
     return true;
+  } else {
+    router.navigateByUrl('/auth/login');
+    return false;
   }
-
-
-  return authService.checkAuthStatus().pipe(
-    map(isLoggedIn => {
-      if (!isLoggedIn) {
-        // Si el token no es válido o no existe, al login
-        router.navigateByUrl('/auth/login');
-        return false;
-      }
-      return true; // Token válido, repobló el Signal y permite el paso
-    })
-  );
 };
