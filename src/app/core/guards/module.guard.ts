63b5f0f9b5d75553632572f7bbf1c93b817e45a9
Leidy <marcelaguerrero5069380@gmail.com>
2026-06-24 13:43:21 -0500

    module.guard.ts

--- src/app/core/guards/module.guard.ts
+++ src/app/core/guards/module.guard.ts
@@ -2,14 +2,12 @@ import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { Auth } from '../services/auth';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { map, of } from 'rxjs';
 
 export const moduleGuard: CanActivateFn = (route, state) => {
   const authService = inject(Auth);
   const router = inject(Router);
   const snackBar = inject(MatSnackBar);
 
-  // Solo nos interesa el nombre del módulo definido en app.routes.ts
   const requiredModule = route.data['module'] as string | undefined;
 
   const showAccessDenied = (message: string) => {
@@ -27,13 +25,10 @@ export const moduleGuard: CanActivateFn = (route, state) => {
       return false;
     }
 
-    // Obtenemos los módulos que el ADMIN le asignó en la BD
     const userModuleNames = authService.userModules().map(m => m.toLowerCase());
 
-    // Si la ruta no pide un módulo específico, lo dejamos pasar (ej: inicio)
     if (!requiredModule) return true;
 
-    // VALIDACIÓN DINÁMICA: ¿El módulo requerido está en la lista de la BD de este usuario?
     const hasModule = userModuleNames.includes(requiredModule.toLowerCase());
 
     if (!hasModule) {
@@ -45,20 +40,11 @@ export const moduleGuard: CanActivateFn = (route, state) => {
     return true;
   };
 
-  // MANEJO DE ASINCRONÍA (Importante para evitar el 404 al refrescar)
+  // Verificamos de forma inmediata
   if (authService.isAuthenticated()) {
     return validateAccess();
+  } else {
+    router.navigateByUrl('/auth/login');
+    return false;
   }
-
-  // Si no está autenticado (refresco de página), esperamos a que el servicio responda
-  return authService.checkAuthStatus().pipe(
-    map(isLoggedIn => {
-      if (isLoggedIn) {
-        return validateAccess();
-      } else {
-        router.navigateByUrl('/auth/login');
-        return false;
-      }
-    })
-  );
 };
