5b3725e2b3f180a8cba4a4e178b259a65d5df4aa
Leidy <marcelaguerrero5069380@gmail.com>
2026-07-01 13:01:56 -0500

    cerrar sesión

--- src/app/core/components/admin-layout/admin-layout.ts
+++ src/app/core/components/admin-layout/admin-layout.ts
@@ -83,7 +83,7 @@ export class AdminLayoutComponent {
 
  logout(): void {
     this.authService.logout();
-    this.router.navigate(['/auth/login']).then(() => {
+    this.router.navigate(['/auth']).then(() => {
       window.location.reload();
     });
   }
